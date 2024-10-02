ccb366c44198e1c2c0938da207ca03a98a6e779d
AmmaraRana <121251290+AmmaraRana@users.noreply.github.com>
2024-10-02 20:42:08 +0500

    Update api.ts

--- src/state/api.ts
+++ src/state/api.ts
@@ -1,9 +1,10 @@
-import { createApi,fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
 
-export const api=createApi({
-baseQuery:fetchBaseQuery({baseUrl:process.env.NEXT_PUBLIC_API_BASE_URL}),
-reducerPath:"api",
-tagTypes:[],
-endpoints: (build) => ({}),
+export const api = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
+  reducerPath: "api",
+  tagTypes: [],
+  endpoints: () => ({}),
 });
-export const{}=api;
+
+export const {} = api;
