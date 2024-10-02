3fbb3514405b8b2573964e079786ffcd66b6ed48
AmmaraRana <121251290+AmmaraRana@users.noreply.github.com>
2024-10-02 20:49:55 +0500

    Update _document.tsx

--- src/app/dashboard/_document.tsx
+++ src/app/dashboard/_document.tsx
@@ -1,20 +1,24 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-const MyDocument = () => (
-  <Html>
-    <Head>
-      <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-        integrity="sha384-DyZvYc5sLd1+I4be9OWF26CqUeLgx1eSfj3EEh8OH8hO0m6OqFs5mTLu4H0OBGso"
-        crossOrigin="anonymous"
-      />
-    </Head>
-    <body>
-      <Main />
-      <NextScript />
-    </body>
-  </Html>
-);
+class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link
+                        rel="stylesheet"
+                        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+                        integrity="sha384-DyZvYc5sLd1+I4be9OWF26CqUeLgx1eSfj3EEh8OH8hO0m6OqFs5mTLu4H0OBGso"
+                        crossOrigin="anonymous"
+                    />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
 
 export default MyDocument;
